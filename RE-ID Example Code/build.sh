b2e849d0ccd1f6a2961895f8572a0f5f744a19b7
JJ1972Kim <119487145+JJ1972Kim@users.noreply.github.com>
2022-12-22 12:14:29 +0200

    Update build.sh

--- RE-ID Example Code/build.sh
+++ RE-ID Example Code/build.sh
@@ -6,7 +6,6 @@ for ARCH in x86_64
 do
     echo "-I- Building ${ARCH}"
     mkdir -p build/${ARCH}
-    # HAILORT_VER=4.9.0 cmake -H. -Bbuild/${ARCH} -DARCH=${ARCH} -DCMAKE_C_COMPILER=${COMPILER[${ARCH}]} -DCMAKE_PREFIX_PATH=/local/users/omerw/.local/share/cmake
     HAILORT_VER=4.10.0 cmake -H. -Bbuild/${ARCH} -DARCH=${ARCH} -DCMAKE_C_COMPILER=${COMPILER[${ARCH}]}
     cmake --build build/${ARCH}
 done
