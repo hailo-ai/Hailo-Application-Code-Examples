cd51a4b83f4a350fc5d08e58d5f95639a136a1a0
omerwer <58146682+omerwer@users.noreply.github.com>
2023-07-27 09:39:08 +0300

    Update get_hefs_and_video.sh

--- runtime/cpp/yolov5_yolov7_detection/get_hefs_and_video.sh
+++ runtime/cpp/yolov5_yolov7_detection/get_hefs_and_video.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.6.0/yolov7.hef
 wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.6.0/yolov5m_wo_spp.hef
+wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/yolov5m_wo_spp_60p_nms_on_hailo.hef
 wget https://hailo-tappas.s3.eu-west-2.amazonaws.com/v3.22/general/media/full_mov_slow.mp4
