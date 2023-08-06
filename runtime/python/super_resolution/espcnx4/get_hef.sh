755c4ebd0bf5b8084998b5db69c42e836a397d8f
Ronit Shpoliansky <ronits@hailo.ai>
2023-08-06 13:49:34 +0300

    update hefspaths

--- runtime/python/super_resolution/espcnx4/get_hef.sh
+++ runtime/python/super_resolution/espcnx4/get_hef.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/hailo8-hefs/espcn_x4_540x960.hef
+wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8/espcn_x4_540x960.hef
