e089e174a4363a8aad603ceb348d41fcd38653e1
Omer Wertheimer <omerw@hostname.newdomain.local>
2023-10-29 14:33:44 +0200

    add yolov8 support for camera input

--- runtime/cpp/yolov8/x86_64/build/x86_64/CMakeFiles/vstream_yolov8_example_cpp.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for vstream_yolov8_example_cpp.
-# This may be replaced when dependencies are built.
--- runtime/cpp/yolov8/x86_64/build/x86_64/CMakeFiles/vstream_yolov8_example_cpp.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for vstream_yolov8_example_cpp.
