5c4811929ca204f47f6743c9da2a27c0f303a258
nina-vilela <marina_vilelab@hotmail.com>
2023-11-05 14:46:31 +0200

    add depth_estimation example

---
+++ runtime/cpp/depth_estimation/download_files.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.9.0/scdepthv3.hef
+wget https://hailo-tappas.s3.eu-west-2.amazonaws.com/v3.22/general/media/instance_segmentation.mp4 -O input_video.mp4
