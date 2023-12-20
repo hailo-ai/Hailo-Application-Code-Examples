7160c4aa4a8bfdbbd6d5cfa65f0a0db40c4871b1
Omer Wertheimer <omerw@had-1042-dsk.qb.hailotech>
2023-12-20 14:19:02 +0200

    general scheduler cpp example

---
+++ runtime/cpp/general/scheduler_example/get_hefs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.6.0/yolov7.hef
+wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.6.0/yolov5m_wo_spp.hef
+wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.4.0/resnet_v1_50.hef
