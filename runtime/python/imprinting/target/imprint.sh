87f9495e8ffedd67d4cdbf04135250572052c431
Alex Finkel <alexf@had-1127-dsk.qb.hailotech>
2024-03-28 16:13:54 +0200

    minor - removed hef arg

--- runtime/python/imprinting/target/imprint.sh
+++ runtime/python/imprinting/target/imprint.sh
@@ -1,8 +1,7 @@
 bash gst_hailo_infer_folder.sh ../training_data/001*
 bash gst_hailo_infer_folder.sh ../training_data/002.*
 
-IMPRINT_CMD="python3 ./imprint_weights_from_txt.py  \
-                  --hef ../hef/resnet_v1_18_featext.hef \
+IMPRINT_CMD="python3 ./imprint_weights_from_txt.py  \                  
                   --npz-input ../onnx/resnet_v1_18_fc.npz \
                   --npz-output fc \
                   --input-images ../training_data/ "
