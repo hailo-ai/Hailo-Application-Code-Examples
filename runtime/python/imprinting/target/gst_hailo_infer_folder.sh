71ca933dd020e4d05a85d297d5a68541ab485bb7
Alex Finkel <alexf@had-1127-dsk.qb.hailotech>
2024-03-28 15:38:47 +0200

    relative paths in bash

--- runtime/python/imprinting/target/gst_hailo_infer_folder.sh
+++ runtime/python/imprinting/target/gst_hailo_infer_folder.sh
@@ -11,9 +11,9 @@ fi
 PIPELINE="gst-launch-1.0 \
           multifilesrc location=$FOLDER/%04d.jpg \
           ! jpegdec ! videoscale ! video/x-raw,width=224,height=224 ! videoconvert \
-          ! hailonet hef-path=/home/root/pocs/online_training/hef/resnet_v1_18_featext.hef \
+          ! hailonet hef-path=resnet_v1_18_featext.hef \
           ! hailofilter function-name=filter \
-               so-path=/home/root/pocs/online_training/target/libfeature_extractor_cross_compilation_h15.so 
+               so-path=./libfeature_extractor_cross_compilation_h15.so 
 	        ! fakesink"
 
 
