d2d53cc6261224e7913e86da2b2d8ec0965e649e
Omer Wertheimer <omerw@had-1042-dsk.qb.hailotech>
2024-08-08 13:23:12 +0300

    delete some stuff

---
+++ runtime/cpp/object_detection/async_detection_inference/build/x86_64/CMakeFiles/vstream_async_detection_example_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vstream_async_detection_example_cpp.dir/common/yolo_hailortpp.cpp.o"
+  "CMakeFiles/vstream_async_detection_example_cpp.dir/common/yolo_hailortpp.cpp.o.d"
+  "CMakeFiles/vstream_async_detection_example_cpp.dir/detection_async.cpp.o"
+  "CMakeFiles/vstream_async_detection_example_cpp.dir/detection_async.cpp.o.d"
+  "vstream_async_detection_example_cpp"
+  "vstream_async_detection_example_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vstream_async_detection_example_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
