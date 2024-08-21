cd0de206d5238b5853076dbf037c910ef9dc03a2
LihiStricher <lihi.stricher@gmail.com>
2024-08-21 14:45:12 +0300

    fix onnxruntime example

--- runtime/cpp/onnxruntime/get_hef_and_onnx.sh
+++ runtime/cpp/onnxruntime/get_hef_and_onnx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8/yolov5m_wo_spp_60p_nms_on_hailo.hef
-wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/onnxs/yolov5m_wo_spp_postprocess.onnx
+wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8/yolov5m_wo_spp.hef
+wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/onnxs/yolov5m_wo_spp_postprocess_v1.onnx
 
 if [ -d "/opt/onnxruntime-linux-x64" ]; then
 	echo "ONNXRuntime folder already exists in /opt. No need to download and extract."
