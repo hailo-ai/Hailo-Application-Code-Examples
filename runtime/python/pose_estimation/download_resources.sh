de6f54b3b4a26fb2081d6fd07e874604471c9734
Oren Yacouel <oreny@hlil-476-lap.qb.hailotech>
2024-09-11 11:09:41 +0300

    implemented task using async api, multiprocessing

---
+++ runtime/python/pose_estimation/download_resources.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+wget https://ultralytics.com/images/zidane.jpg
+wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8/yolov8s_pose_v2.hef
--- runtime/python/pose_estimation/get_sources.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-wget https://ultralytics.com/images/zidane.jpg
-wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/images/person.jpg
-# H8 HEF:
-wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8/yolov8s_pose_mz.hef
-# H8L HEF:
-wget https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_pose_h8l_pi.hef
