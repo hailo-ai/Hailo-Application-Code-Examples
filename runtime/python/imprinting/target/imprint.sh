df6f1a182438f336bcf0b0dca44da7113e3cc8f2
Alex Finkel <alexf@had-1127-dsk.qb.hailotech>
2025-10-16 11:55:56 +0300

    removed old

--- runtime/python/imprinting/target/imprint.sh
+++
@@ -1,9 +0,0 @@
-bash gst_hailo_infer_folder.sh ../training_data/001*
-bash gst_hailo_infer_folder.sh ../training_data/002.*
-
-IMPRINT_CMD="python3 ./imprint_weights_from_txt.py \
-	            --npz-input resnet_v1_18_fc.npz \
-                --npz-output fc \
-                --input-images ../training_data/ "
-
-eval ${IMPRINT_CMD}
